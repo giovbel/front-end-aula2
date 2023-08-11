50fb99b75bc4ba11278349e65235aea12dfb6e1c
GIOVANNA SILVA BELO <23111786@CFP127ALUNO.LOCAL>
2023-08-11 12:24:14 -0300

    terminando

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const forma1 = document.getElementById('forma1')
+const forma2 = document.getElementById('forma2')
+const forma3 = document.getElementById('forma3')
+const forma4 = document.getElementById('forma4')
+const forma5 = document.getElementById('forma5')
+const forma6 = document.getElementById('forma6')
+const fundo = document.getElementById('fundo')
+const forma7 = document.getElementById('forma7')
+
+function mudarCor(){
+    forma1.classList.toggle('amarelo')
+}
+
+function mudarForma(){
+    forma2.classList.toggle('circulo')
+}
+
+function mudarImagem(){
+    forma3.classList.toggle('melanie')
+}
+
+function desaparecer(){
+    forma4.classList.toggle('desaparecer')
+}
+
+function girando(){
+    forma5.classList.toggle('round')
+}
+
+function mudarFundo(){
+    fundo.classList.toggle('corDeFundo')
+}
+
+function mudarTexto(){
+
+    if(surprise.textContent === 'surprise'){
+        surprise.textContent = 'PIKACHUU !!'
+        forma7.classList.add('pikachuimg')
+    }else{
+        surprise.textContent = 'surprise'
+        forma7.classList.remove('pikachuimg')
+    }
+}
+
+forma1.addEventListener('click', mudarCor)
+forma2.addEventListener('click', mudarForma)
+forma3.addEventListener('click', mudarImagem)
+forma4.addEventListener('click', desaparecer)
+forma5.addEventListener('click', girando)
+forma6.addEventListener('click', mudarFundo)
+forma7.addEventListener('click', mudarTexto)
